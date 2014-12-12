4afb79ac2c73cadf2a913566b16b364a06e9b0f5
boboge <524895963@qq.com>
2014-12-12 17:31:45 +0800

    资源

---
+++ FellowY/src/com/bobo/service/BootBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.bobo.service;
+
+import com.bobo.view.MainActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+	static final String action_boot="android.intent.action.BOOT_COMPLETED";
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	      if (intent.getAction().equals(action_boot)){
+	            Intent ootStartIntent=new Intent(context,MainActivity.class);
+	            ootStartIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	            context.startActivity(ootStartIntent);
+	        }		
+	}
+
+}
---
+++ FellowY/src/com/bobo/view/MainActivity.java
@@ -0,0 +1,55 @@
+package com.bobo.view;
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
+
+import com.bobo.R;
+
+public class MainActivity extends TabActivity{
+	long startTime;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		TabHost tabHost = getTabHost();
+		TabSpec spec1 = tabHost
+				.newTabSpec("one")
+				.setIndicator("父母",getResources().getDrawable(R.drawable.me))
+				.setContent( new Intent(this, MainActivity.class));
+		TabSpec spec2 = tabHost
+				.newTabSpec("two")
+				.setIndicator("子女",getResources().getDrawable(R.drawable.you))
+				.setContent( new Intent(this, MainActivity.class));
+		tabHost.addTab(spec1);
+		tabHost.addTab(spec2);
+		tabHost.setCurrentTab(0);
+		
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			long diffTime = System.currentTimeMillis() - startTime;
+			if (diffTime > 1000) {
+				Toast.makeText(MainActivity.this, "再按一次将退出程序",
+						Toast.LENGTH_SHORT).show();
+				startTime = System.currentTimeMillis();
+				return true;
+			} else {
+				System.exit(0);
+				return true;
+			}
+		} else {
+			return super.onKeyDown(keyCode, event);
+		}
+	}
+
+}
